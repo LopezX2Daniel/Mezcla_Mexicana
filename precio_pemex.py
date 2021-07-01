987f5e1f8857b7e025e6ccad0441ca12c8bb4b31
Daniel Eduardo López <64986476+LopezX2Daniel@users.noreply.github.com>
2021-06-30 23:09:31 -0500

    Update precio_pemex.py

--- precio_pemex.py
+++ precio_pemex.py
@@ -1,9 +1,9 @@
 import requests
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup as bs
     
 url = "https://www.pemex.com/Paginas/default.aspx"
 serveresponse = requests.get(url)
-soup = BeautifulSoup(serveresponse.content, "html.parser")
+soup = bs(serveresponse.content, "html.parser")
 divprecio = soup.findAll("div", {"class": "left precio-mezcla"})
 divprecio = str(divprecio).replace('[<div class="left precio-mezcla">','')
 divprecio = divprecio.replace("*</div>]","") + " USD"
